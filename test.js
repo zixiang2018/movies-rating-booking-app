027a861d3c0f7721de0c4d652bc794e2de094487
Zi Xiang <zixiang.tng.2018@sis.smu.edu.sg>
2020-12-30 00:20:20 +0800

    Refactor: test cases were wrongly named
    - Moved each test cases into its describe block
    - Proper naming of the testcases

--- test.js
+++ test.js
@@ -18,9 +18,9 @@ let movie;
 
 
 
-describe("GET /api/movies", () => {
+describe("Movies", () => {
 
-    before ((done)=>{
+    beforeEach ((done)=>{
         for(var i = 1; i <= 5; i++){
             movie = Movie.create({
                 title: "testTitle"+i.toString(),
@@ -32,91 +32,101 @@ describe("GET /api/movies", () => {
         done()        
     })
 
-
-    it("It should return all movies", (done) => {
-        chai.request(app)
-            .get("/api/movies")
-            .end((err, res) => {
-                expect(res).to.have.status(200);
-                expect(res.body).to.be.a('Object')
-                var expected = {
-                    1: {
-                      actors: [ 'testActor1', 'testActor2', 'testActor3' ],
-                      title: 'testTitle1',
-                      rating: '1 based on 200,000 user ratings',
-                      thumbnail_url: 'https://www.google.com',
-                      id: '1'
-                    },
-                    2: {
-                      actors: [ 'testActor1', 'testActor2', 'testActor3' ],
-                      title: 'testTitle2',
-                      rating: '2 based on 200,000 user ratings',
-                      thumbnail_url: 'https://www.google.com',
-                      id: '2'
-                    },
-                    3: {
-                      actors: [ 'testActor1', 'testActor2', 'testActor3' ],
-                      title: 'testTitle3',
-                      rating: '3 based on 200,000 user ratings',
-                      thumbnail_url: 'https://www.google.com',
-                      id: '3'
-                    },
-                    4: {
-                      actors: [ 'testActor1', 'testActor2', 'testActor3' ],
-                      title: 'testTitle4',
-                      rating: '4 based on 200,000 user ratings',
-                      thumbnail_url: 'https://www.google.com',
-                      id: '4'
-                    },
-                    5: {
-                      actors: [ 'testActor1', 'testActor2', 'testActor3' ],
-                      title: 'testTitle5',
-                      rating: '5 based on 200,000 user ratings',
-                      thumbnail_url: 'https://www.google.com',
-                      id: '5'
+    /*
+    * Test the GET /api/movies/ route
+    */
+   describe ("/GET movies",()=>{
+        it("It should return all movies", (done) => {
+            chai.request(app)
+                .get("/api/movies")
+                .end((err, res) => {
+                    expect(res).to.have.status(200);
+                    expect(res.body).to.be.a('Object')
+                    var expected = {
+                        1: {
+                        actors: [ 'testActor1', 'testActor2', 'testActor3' ],
+                        title: 'testTitle1',
+                        rating: '1 based on 200,000 user ratings',
+                        thumbnail_url: 'https://www.google.com',
+                        id: '1'
+                        },
+                        2: {
+                        actors: [ 'testActor1', 'testActor2', 'testActor3' ],
+                        title: 'testTitle2',
+                        rating: '2 based on 200,000 user ratings',
+                        thumbnail_url: 'https://www.google.com',
+                        id: '2'
+                        },
+                        3: {
+                        actors: [ 'testActor1', 'testActor2', 'testActor3' ],
+                        title: 'testTitle3',
+                        rating: '3 based on 200,000 user ratings',
+                        thumbnail_url: 'https://www.google.com',
+                        id: '3'
+                        },
+                        4: {
+                        actors: [ 'testActor1', 'testActor2', 'testActor3' ],
+                        title: 'testTitle4',
+                        rating: '4 based on 200,000 user ratings',
+                        thumbnail_url: 'https://www.google.com',
+                        id: '4'
+                        },
+                        5: {
+                        actors: [ 'testActor1', 'testActor2', 'testActor3' ],
+                        title: 'testTitle5',
+                        rating: '5 based on 200,000 user ratings',
+                        thumbnail_url: 'https://www.google.com',
+                        id: '5'
+                        }
                     }
-                }
-                  let i = 1
-                  for (const [key, value] of Object.entries(res.body)){
-                    expect(value.actors).to.be.eql(expected[i].actors)
-                    expect(value.title).to.be.eql(expected[i].title)
-                    expect(value.rating).to.be.eql(expected[i].rating)
-                    expect(value.thumbnail_url).to.be.eql(expected[i++].thumbnail_url)
+                    let i = 1
+                    for (const [key, value] of Object.entries(res.body)){
+                        expect(value.actors).to.be.eql(expected[i].actors)
+                        expect(value.title).to.be.eql(expected[i].title)
+                        expect(value.rating).to.be.eql(expected[i].rating)
+                        expect(value.thumbnail_url).to.be.eql(expected[i++].thumbnail_url)
 
-                  }
-                
-                done()
-            })
-    })
-
-    it("It should return all movies", (done) => {
-        chai.request(app)
-            .get("/api/movies/testTitle1")
-            .end((err, res) => {
-                expect(res).to.have.status(200);
-                expect(res.body).to.be.a('array')
-                var expected = {
-                    1: {
-                      actors: [ 'testActor1', 'testActor2', 'testActor3' ],
-                      title: 'testTitle1',
-                      rating: '1 based on 200,000 user ratings',
-                      thumbnail_url: 'https://www.google.com',
-                      id: '1'
                     }
-                }
-                  let i = 1
-                  for (const [key, value] of Object.entries(res.body)){
-                    expect(value.actors).to.be.eql(expected[i].actors)
-                    expect(value.title).to.be.eql(expected[i].title)
-                    expect(value.rating).to.be.eql(expected[i].rating)
-                    expect(value.thumbnail_url).to.be.eql(expected[i++].thumbnail_url)
-                  }
-                
-                done()
-            })
-    })
+                    
+                    done()
+                })
+        })
+   })
+    
 
-    after( (done) => {
+    /*
+    * Test the GET /api/movies/:title route
+    */
+   describe("/GET movie",()=>{
+        it("It should return one movie", (done) => {
+            chai.request(app)
+                .get("/api/movies/testTitle1")
+                .end((err, res) => {
+                    expect(res).to.have.status(200);
+                    expect(res.body).to.be.a('array')
+                    var expected = {
+                        1: {
+                        actors: [ 'testActor1', 'testActor2', 'testActor3' ],
+                        title: 'testTitle1',
+                        rating: '1 based on 200,000 user ratings',
+                        thumbnail_url: 'https://www.google.com',
+                        id: '1'
+                        }
+                    }
+                    let i = 1
+                    for (const [key, value] of Object.entries(res.body)){
+                        expect(value.actors).to.be.eql(expected[i].actors)
+                        expect(value.title).to.be.eql(expected[i].title)
+                        expect(value.rating).to.be.eql(expected[i].rating)
+                        expect(value.thumbnail_url).to.be.eql(expected[i].thumbnail_url)
+                    }
+                    done()
+                })
+        })
+   })
+    
+
+    afterEach( (done) => {
          Movie.remove({}).then(() => done())
       })
 })
