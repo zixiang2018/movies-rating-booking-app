322184113983d9f02915ecfed5ad7f7829fbe367
Zi Xiang <zixiang.tng.2018@sis.smu.edu.sg>
2020-12-30 23:20:15 +0800

    Feat: Displayed all movies on Frontend using React

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+import ViewAllMovies from "./components/view-all-movies.component";
+
+class App extends Component {
+  render(){
+    return(
+      <Router>
+        <div className="container">
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+              <Link to="/" className="navbar-brand">Movie Galore</Link>
+            </nav>
+
+            <Route path="/" exact component={ViewAllMovies} />
+        </div>
+
+         
+      </Router>
+    )
+  }
 }
 
 export default App;
---
+++ client/src/components/view-all-movies.component.js
@@ -0,0 +1,64 @@
+import React, {Component} from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+const Movie = props => (
+    <tr>
+        <td><img src={props.movie.thumbnail_url}/></td>
+        <td>{props.movie.title}</td>
+        <td>{props.movie.movie_year}</td>
+        <td>{props.movie.rating}</td>
+        <td>{props.movie.num_of_ratings}</td>
+        <td>{props.movie.actors.map((actor, i)=>
+            <tr>{actor}</tr>
+        )}</td>
+    </tr>
+)
+
+export default class MovieList extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {movies: []};
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:5000/api/movies/')
+            .then(response => {
+                // console.log(response.data)
+                this.setState({movies: response.data});
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+    }
+
+    movieList() {
+        return Object.keys(this.state.movies).map((k, i) => {
+            return <Movie movie={this.state.movies[k]} key={i} />;
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h3>Movies</h3>
+                <table className="table table-striped" style={{ marginTop: 20 }}>
+                    <thead>
+                        <tr>
+                            <th>Banner</th>
+                            <th>Title</th>
+                            <th>Year</th>
+                            <th>Rating</th>
+                            <th>Number of Ratings</th>
+                            <th>Actors</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        { this.movieList() }
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
+}
