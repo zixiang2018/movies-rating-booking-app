b8f4ca0b3a56e2c77917add1d451f4557c219342
Zi Xiang <zixiang.tng.2018@sis.smu.edu.sg>
2021-01-08 00:24:35 +0800

    Deployment: Heroku Deployment

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ const path = require('path')
 const app = express();
 
 // Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, 'client/build')))
+app.use(express.static(path.join(__dirname, 'client/build')));
 // Anything that doesn't match the above, send back index.html
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'))
-})
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname + '/client/build/index.html'))
+// })
 
 app.use(cors());
 
@@ -20,6 +20,11 @@ app.use(bodyParser.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+  });
 
 // Connect to the database
 const db = require("./src/models");
@@ -45,6 +50,5 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 });
-
 
 module.exports = app
--- client/src/components/movieTable.component.js
+++ client/src/components/movieTable.component.js
@@ -5,6 +5,7 @@ import DataTable from 'react-data-table-component';
 
 
 const MoviePage = ({movieList=[]}) => {
+    console.log(movieList)
     const columns = [
         {
           name: 'Banner',
